6797527ee6d8412170d2c7adf2d0655c0d554177
XenoPhex <AnandGaitonde@GMail.com>
2023-02-10 17:01:01 -0800

    add cmp-nvim-lsp defaults

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -3,26 +3,27 @@ if not status_ok then
 	return
 end
 
+local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok then
+	return
+end
+
 local function extended_capabilities()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	capabilities.textDocument.completion.completionItem.preselectSupport = true
-	capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-	capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-	capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-	capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-	capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-	capabilities.textDocument.completion.completionItem.resolveSupport = {
-		properties = { "documentation", "detail", "additionalTextEdits" },
-	}
-	-- Tell the server the capability of foldingRange,
-	-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-	capabilities.textDocument.foldingRange = {
-		dynamicRegistration = false,
-		lineFoldingOnly = true,
-	}
-	return capabilities
+	return tbl.merge(vim.lsp.protocol.make_client_capabilities(), cmp_nvim_lsp.default_capabilities(), {
+		textDocument = {
+			completion = {
+				completionItem = {
+					documentationFormat = { "markdown", "plaintext" },
+				},
+			},
+			-- Tell the server the capability of foldingRange,
+			-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+			foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			},
+		},
+	})
 end
 
 local function load_languages()

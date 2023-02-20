7c9130bdf15ff8db035569c76e60ecb99008c642
XenoPhex <AnandGaitonde@Gmail.com>
2023-02-19 22:34:52 -0800

    safely load anything from lua/custom*

--- init.lua
+++ init.lua
@@ -4,4 +4,4 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("config")
+require("config").setup()
--- lua/config.lua
+++ lua/config.lua
@@ -1,35 +1,52 @@
 local file = require("utils.file")
 
--- Setup Globals
-_G.CONFIG_PATH = vim.fn.stdpath("config")
-_G.DATA_PATH = vim.fn.stdpath("data")
-_G.CACHE_PATH = vim.fn.stdpath("cache")
-_G.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or file.join(os.getenv("HOME"), ".config")
-_G.tbl = require("utils.table")
-_G.global_settings = {
-	theme = "catppuccin",
-	log = { level = "INFO" },
-}
+local M = {}
 
--- Configure python based on the virtualenv
-local virtualenv = os.getenv("VIRTUAL_ENV")
-if virtualenv then
-	vim.g["python3_host_prog"] = virtualenv .. "/bin/python"
+M.setup_globals = function()
+	-- Setup Globals
+	_G.CONFIG_PATH = vim.fn.stdpath("config")
+	_G.DATA_PATH = vim.fn.stdpath("data")
+	_G.CACHE_PATH = vim.fn.stdpath("cache")
+	_G.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or file.join(os.getenv("HOME"), ".config")
+	_G.tbl = require("utils.table")
+	_G.global_settings = {
+		theme = "catppuccin",
+		log = { level = "INFO" },
+	}
+	-- Configure python based on the virtualenv
+	local virtualenv = os.getenv("VIRTUAL_ENV")
+	if virtualenv then
+		vim.g["python3_host_prog"] = virtualenv .. "/bin/python"
+	end
+
+	_G.override_settings = M.get_overrides()
 end
 
-_G.override_settings = file.load_module("custom") or require("custom_template")
+M.get_overrides = function()
+	local status_ok, overrides = pcall(require, "custom")
+	if not status_ok then
+		return require("custom_template")
+	end
+	return tbl.merge(require("custom_template"), overrides) -- merge custom settings with template in case custom is missing fields
+end
 
-require("options")
+M.setup = function()
+	M.setup_globals()
 
-vim.cmd("set whichwrap+=<,>,[,]")
-vim.cmd([[set iskeyword+=-]])
--- disable open fold with `l`
-vim.cmd([[set foldopen-=hor]])
+	require("options")
 
-vim.o.grepprg = "rg --vimgrep --no-heading --smart-case"
-vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+	vim.cmd("set whichwrap+=<,>,[,]")
+	vim.cmd([[set iskeyword+=-]])
+	-- disable open fold with `l`
+	vim.cmd([[set foldopen-=hor]])
 
-require("plugins")
-require("keyboard")
-require("autocmd")
-vim.notify("Config Loaded!", "info")
+	vim.o.grepprg = "rg --vimgrep --no-heading --smart-case"
+	vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+
+	require("plugins")
+	require("keyboard")
+	require("autocmd")
+	vim.notify("Config Loaded!", "info")
+end
+
+return M

e3c1be528b4fadadce1202caf339928ce775715d
XenoPhex <AnandGaitonde@GMail.com>
2023-03-10 16:08:37 -0500

    fix: sidebar numbers - actually use statuscol properly

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -3,9 +3,14 @@ if not status_ok then
 	return
 end
 
+local builtin = require("statuscol.builtin")
 require("statuscol").setup({
-	foldfunc = "builtin",
-	setopt = true,
+	relculright = true,
+	segments = {
+		{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+		{ text = { "%s" }, click = "v:lua.ScSa" },
+		{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+	},
 })
 
 ufo.setup({

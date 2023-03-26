7ca24af74ff3f0cd3ee821a2d342539604acd003
XenoPhex <AnandGaitonde@GMail.com>
2023-03-25 23:42:26 -0700

    add link for unicode look-up

--- lua/utils/icons.lua
+++ lua/utils/icons.lua
@@ -21,6 +21,8 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
+-- https://www.unicode.org/charts/charindex.html
+
 return {
 	git = {
 		LineAdded = "",

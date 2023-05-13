0ae169d38c9ea6f42338a71e1b39a50d937a56cf
XenoPhex <AnandGaitonde@GMail.com>
2023-05-12 20:03:46 -0700

    chore(neo-tree): manually replace icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -26,7 +26,50 @@ picker.setup({
 	other_win_hl_color = "#e35e4f",
 })
 
+local icons = require("utils.icons").ui
+local gitIcons = require("utils.icons").git
+
 tree.setup({
+	default_component_configs = {
+		icon = {
+			folder_empty = icons.EmptyFolder,
+			folder_empty_open = icons.EmptyFolderOpen,
+		},
+		git_status = {
+			symbols = {
+				added = gitIcons.LineAdded,
+				modified = gitIcons.LineModified,
+				deleted = gitIcons.LineRemoved,
+				renamed = gitIcons.FileRenamed,
+
+				untracked = gitIcons.FileUntracked,
+				ignored = gitIcons.FileIgnored,
+				unstaged = gitIcons.FileUnstaged,
+				staged = gitIcons.FileStaged,
+				conflict = gitIcons.FileUnmerged,
+			},
+		},
+	},
+	document_symbols = {
+		kinds = {
+			File = { icon = icons.File, hl = "Tag" },
+			Namespace = { icon = icons.Namespace, hl = "Include" },
+			Package = { icon = icons.Package, hl = "Label" },
+			Class = { icon = icons.Class, hl = "Include" },
+			Property = { icon = icons.Property, hl = "@property" },
+			Enum = { icon = icons.Field, hl = "@number" },
+			Function = { icon = icons.Function, hl = "Function" },
+			String = { icon = icons.String, hl = "String" },
+			Number = { icon = icons.Number, hl = "Number" },
+			Array = { icon = icons.Array, hl = "Type" },
+			Object = { icon = icons.Object, hl = "Type" },
+			Key = { icon = icons.Key, hl = "" },
+			Struct = { icon = icons.Struct, hl = "Type" },
+			Operator = { icon = icons.Operator, hl = "Operator" },
+			TypeParameter = { icon = icons.TypeParameter, hl = "Type" },
+			StaticMethod = { icon = "󰠄 ", hl = "Function" },
+		},
+	},
 	window = {
 		mapping_options = {
 			noremap = true,

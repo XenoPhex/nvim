cc29ff69838f483c25931a38ada6cdfb66e1e420
XenoPhex <github@xenophex.com>
2024-07-17 01:16:33 -0700

    feat(gopls): update config for latest features

--- lua/plugins/lang/gopls.lua
+++ lua/plugins/lang/gopls.lua
@@ -2,6 +2,7 @@ return {
 	settings = {
 		gopls = {
 			analyses = {
+				fieldalignment = true,
 				nilness = true,
 				shadow = true,
 				unusedparams = true,
@@ -9,17 +10,32 @@ return {
 				unusedwrite = true,
 				useany = true,
 			},
-			experimentalPostfixCompletions = true,
-			gofumpt = true,
+			codelenses = {
+				gc_details = false,
+				generate = true,
+				regenerate_cgo = true,
+				run_govulncheck = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = true,
+			},
 			hints = {
 				assignVariableTypes = true,
 				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
 				constantValues = true,
 				functionTypeParameters = true,
 				parameterNames = true,
 				rangeVariableTypes = true,
 			},
+			completeUnimported = true,
+			directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+			experimentalPostfixCompletions = true,
+			gofumpt = true,
+			semanticTokens = true,
 			staticcheck = false,
+			usePlaceholders = true,
 		},
 	},
 }
